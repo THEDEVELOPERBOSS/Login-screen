a3ec5996c299bf466a18c9ecf6bd6a939341ccc6
THEDEVELOPERBOSS <109550555+THEDEVELOPERBOSS@users.noreply.github.com>
2022-08-16 19:39:52 -0600

    Add files via upload

---
+++ login.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+int main() {
+	// these are the variables
+	int user1 = 0; 
+	int user2 =  0; 
+	int username = 0; 
+	int passwd = 0; 
+	int passwd1 = 0; 
+	int passwd2 = 0; 
+	// this will ask the user for their username and password
+	
+	std::cout << "Enter you username";
+	std::cin >> username;
+	//this will ask for the first users username and password
+	
+	if (username == user1) {
+		
+		std::cout << "Now enter your password"; 
+		std::cin >> passwd; 
+		
+		else {
+			std::cout << "Get out you hacker";
+			}
+		
+		if (passwd == passwd1){
+			std::cout << "Access Granted";
+			}
+	}
+
+		
+}
