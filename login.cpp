291b502ed47362c68ed280de23268f8d95494bb2
THEDEVELOPERBOSS <109550555+THEDEVELOPERBOSS@users.noreply.github.com>
2022-08-19 20:46:51 -0600

    Add files via upload
    This is the final project just edit the Cheese and Falcon variables too your liking then you will have a working login interface in your terminal

--- login.cpp
+++ login.cpp
@@ -1,31 +1,36 @@
 #include<iostream>
+#include<string> 
+using namespace std;
 
 int main() {
 	// these are the variables
-	int user1 = 0; 
-	int user2 =  0; 
-	int username = 0; 
-	int passwd = 0; 
-	int passwd1 = 0; 
-	int passwd2 = 0; 
+	string user1 = "Cheese"; 
+	string user2 =  "Falcon"; 
+	string username = "default"; 
+	string passwd = "default"; 
+	string passwd1 = "default"; 
+	string passwd2 = "default"; 
 	// this will ask the user for their username and password
 	
-	std::cout << "Enter you username\n";
+	std::cout << "Enter you username.\n";
 	std::cin >> username;
-	//this will ask for the first users username and password
+
+	if ((username.compare(user1) == 0) || (username.compare(user2) == 0)) 
+	{ 
 	
-	if (username == user1) {
-		
-		std::cout << "Now enter your password\n"; 
+		std::cout << "Now enter your password\n";
 		std::cin >> passwd;
-		
-	}
-		else if (passwd == passwd1){
-			std::cout << "Access Granted\n";
-			}
+	
+		if (username.compare(user1) == 0) 
+		{
+			passwd1 = passwd;
+			cout << "user" << user1 << "has a password of" << passwd1 << endl;
+		}
 		else {
-			std::cout << "Get out you hacker\n";
-			}
+			passwd2 = passwd; 
+			cout << "user" << user2 << "has a password of" << passwd2 << endl; 
+		}
+	}
 		
 
 
